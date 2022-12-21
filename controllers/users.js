6cfa01f73126f26d5c68594076e962ccbc358235
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2022-12-21 13:42:18 -0800

    added logout, part 2 finisdhed

--- controllers/users.js
+++ controllers/users.js
@@ -39,5 +39,51 @@ router.post('/', async (req, res) => {
     }
 })
 
+// GET / users/login -- render a login form that POsts to /users/login
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+
+//POST / users/login -- input data from form renderred @ GET /users/login
+router.post('/login', async (req, res) => {
+    try {
+        // look up user based on email
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        //boilerplate messageif login fails
+        const badCredentialMessage = 'username or password incorrect'
+        if (!user) {
+            //if user isn't found in the db
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else if(user.password !== req.body.password) {
+            //if the user's supplied password is incorrect 
+            res.redirect('/users/login?message=' + badCredentialMessage)
+            
+        } else {
+            //if the user is found and their password matches log them in
+            console.log('loggin user in')
+            res.cookie('userId', user.id)
+            res.redirect('/')
+        }
+        
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('server error')
+    }
+    // res.send(' check users credentials against db')
+})
+
+// GET /users/logout -- clear any cookies and redirect to the homepage
+router.get('/logout', (req, res) => {
+    //log the user out by removing the cookie
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
 //export
 module.exports = router
