19dc317f729a0c6540b3592b8e6936ff95f2c489
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2022-12-27 14:28:37 -0800

    having trouble with goals db. Can't get rid of an invisible column not defined in model creation and not in models or migrations folders. Need more research

--- controllers/goals.js
+++ controllers/goals.js
@@ -10,5 +10,53 @@ const db = require('../models')
 const API_KEY = process.env.APIKEY
 
 //res.locals.user comes from middleware, tells us if user is logged in and the PK of the user in our database
+//
 
-// GET // users/:userId/goals
+//GET // / -- form with new goal
+router.get('/', (req,res) =>{
+    res.render('goals/new.ejs')
+})
+
+// POST // / --create the new goal
+router.post('/', async (req,res) =>{
+    try {
+        //find or create a goal
+        const [newGoal, createdGoal] = await db.goal.findOrCreate({
+            where: {
+                name: req.body.name.toLowerCase()
+            }, 
+            defaults: {
+            description: req.body.description,
+            due_date: req.body.dueDate
+            }
+        })
+        //if goal exists, redirect to goals page
+        // if (!createdGoal) {
+        //     res.redirect('users/profile/?message=That goal already exists.')
+        // } else {
+            //if we create a new goal, redirect to that page here
+            res.redirect(`/user/profile`)
+        // }
+    } catch (err) {
+        console.log('this is a post error', err)
+    }
+})
+
+// GET // /:goalsId shows a specific page for a goal
+router.get('/:goalsId', async (req,res) => {
+    // console.log(res.locals.user)
+    let user = res.locals.user.email
+
+    if(res.locals.user) {
+        res.render('goals/show.ejs', {
+            user: user
+        })
+    } else {
+        res.redirect('/users/login?message=You must authenticate before you can view this resource!')
+    }
+})
+
+
+
+//export
+module.exports = router
--- models/goal.js
+++ models/goal.js
@@ -11,11 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.goal.belongsTo(models.user)
-      models.goal.hasMany(models.task)
     }
   }
-  
   goal.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING,
